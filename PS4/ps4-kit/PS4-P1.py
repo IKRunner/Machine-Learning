167c78373a1d1f2f890c2b47f38feb773b82af43
“IKRunner” <iachilihu@gmail.com>
2021-04-11 10:58:19 -0400

    Update PS4-P1.py

--- PS4/ps4-kit/PS4-P1.py
+++ PS4/ps4-kit/PS4-P1.py
@@ -7,46 +7,46 @@ from numpy import genfromtxt
 
 
 
-def plot_reconstruction(images, title, filename):
-    '''
-    Plots 10%, 20%, ..., 100% reconstructions of a 28x28 image
-
-    Args
-        images (numpy.array)
-            images has size (10, 28, 28)
-        title (str)
-            title within the image
-        filename (str)
-            name of the file where the image is saved
-
-    Returns
-        None
-
-    Example usage:
-        >>> images = np.zeros(10,28,28)
-        >>> images[0,:,:] = x10.reshape((28,28))
-        >>> images[1,:,:] = x20.reshape((28,28))
-        >>> ...
-        >>> images[9,:,:] = x100.reshape((28,28))
-        >>> utils.plot_reconstruction(images, 'Image Title', 'filename.png')
-    '''
-    assert images.shape == (10,28,28)
-    fig, (
-        (ax0, ax1, ax2, ax3),
-        (ax4, ax5, ax6, ax7),
-        (ax8, ax9, _, _)
-    ) = plt.subplots(3, 4)
-    axes = [ax9, ax8, ax7, ax6, ax5, ax4, ax3, ax2, ax1, ax0]
-    percents = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    for i in range(10):
-        ax = axes[i]
-        percent_name = f'{percents[i]}%' if i != 9 else 'Original'
-        ax.set(title=percent_name)
-        axes[i].imshow(images[i,:,:], cmap='gray')
-    fig.suptitle(title)
-    plt.tight_layout()
-    plt.savefig(filename)
-
+# def plot_reconstruction(images, title, filename):
+#     '''
+#     Plots 10%, 20%, ..., 100% reconstructions of a 28x28 image
+#
+#     Args
+#         images (numpy.array)
+#             images has size (10, 28, 28)
+#         title (str)
+#             title within the image
+#         filename (str)
+#             name of the file where the image is saved
+#
+#     Returns
+#         None
+#
+#     Example usage:
+#         >>> images = np.zeros(10,28,28)
+#         >>> images[0,:,:] = x10.reshape((28,28))
+#         >>> images[1,:,:] = x20.reshape((28,28))
+#         >>> ...
+#         >>> images[9,:,:] = x100.reshape((28,28))
+#         >>> utils.plot_reconstruction(images, 'Image Title', 'filename.png')
+#     '''
+#     assert images.shape == (10,28,28)
+#     fig, (
+#         (ax0, ax1, ax2, ax3),
+#         (ax4, ax5, ax6, ax7),
+#         (ax8, ax9, _, _)
+#     ) = plt.subplots(3, 4)
+#     axes = [ax9, ax8, ax7, ax6, ax5, ax4, ax3, ax2, ax1, ax0]
+#     percents = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+#     for i in range(10):
+#         ax = axes[i]
+#         percent_name = f'{percents[i]}%' if i != 9 else 'Original'
+#         ax.set(title=percent_name)
+#         axes[i].imshow(images[i,:,:], cmap='gray')
+#     fig.suptitle(title)
+#     plt.tight_layout()
+#     plt.savefig(filename)
+#
 
 ################################################
 # Problem 1a(i)
@@ -57,5 +57,5 @@ print('Load.........)')
 # t = utils.load_all_train_test_data("P1/X_train.csv")
 my_data = genfromtxt('P1/X_train.csv', delimiter=',')
 last_example = np.reshape(my_data[-1, :], (28, 28))
-plt.imshow(last_example, cmap="gray")
+t = plt.imshow(last_example, cmap="gray")
 p=1
